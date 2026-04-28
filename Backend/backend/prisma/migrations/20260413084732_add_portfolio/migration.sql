a5d33980b1a082e6125ba24e489ccb8bc5b5c4d4
abdullahakhlaque <ansari.abdullah@somaiya.edu>
2026-04-28 16:13:33 +0530

    fix prisma schema for postgresql

--- Backend/backend/prisma/migrations/20260413084732_add_portfolio/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Portfolio" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- Backend/backend/prisma/migrations/20260415181631_add_blog_media/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Blog" ADD COLUMN "mediaType" TEXT;
-ALTER TABLE "Blog" ADD COLUMN "mediaUrl" TEXT;
